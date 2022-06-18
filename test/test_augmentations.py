563c1852edcebf7fe3e50cf3706719de9ef09ab1
DevinDeSilva <devin.18@cse.mrt.ac.lk>
2022-06-18 14:46:56 +0530

    [change] correcting the tests and config files to handle alpha numeric characters

--- test/test_augmentations.py
+++ test/test_augmentations.py
@@ -16,60 +16,60 @@ class TestNoiseAugmentation(unittest.TestCase):
         self.config["common_configs"]["font_file_loc"] = self.font_type.get_font_file_location()
 
     def test_gaussian_noise(self):
-        configurations = [DigitConfig.load_config(self.config, x) for x in "123 758abc3847 475845 5849"]
+        configurations = [DigitConfig.load_config(self.config, x, 0) for x in "123 758abc3847 475845 5849"]
         digits = DigitSequence(configs=configurations)
         array, annotation = digits.data()
         test_annotations(array, annotation)
 
-        configurations = [DigitConfig.load_config(self.config, x) for x in "123 75838iilco47 475845 5849"]
+        configurations = [DigitConfig.load_config(self.config, x, 0) for x in "123 75838iilco47 475845 5849"]
         digits = DigitSequence(configs=configurations, augmentations=[GaussianNoise()])
         array, annotation = digits.data()
         test_annotations(array, annotation)
         self.assertTrue(np.unique(array).shape[0] > 2)
 
     def test_salt_pepper_noise(self):
-        configurations = [DigitConfig.load_config(self.config, x) for x in "123 758vburb3847 475845 5849"]
+        configurations = [DigitConfig.load_config(self.config, x, 0) for x in "123 758vburb3847 475845 5849"]
         digits = DigitSequence(configs=configurations)
         array, annotation = digits.data()
         test_annotations(array, annotation)
 
-        configurations = [DigitConfig.load_config(self.config, x) for x in "123 7583847 475845 5849"]
+        configurations = [DigitConfig.load_config(self.config, x, 0) for x in "123 7583847 475845 5849"]
         digits = DigitSequence(configs=configurations, augmentations=[SPNoise()])
         array, annotation = digits.data()
         test_annotations(array, annotation)
         self.assertTrue(np.unique(array).shape[0] > 2)
 
     def test_speckle_noise(self):
-        configurations = [DigitConfig.load_config(self.config, x) for x in "123 7583847 475845 5849"]
+        configurations = [DigitConfig.load_config(self.config, x, 0) for x in "123 7583847 475845 5849"]
         digits = DigitSequence(configs=configurations)
         array, annotation = digits.data()
         test_annotations(array, annotation)
 
-        configurations = [DigitConfig.load_config(self.config, x) for x in "123 7583847 475845 5849"]
+        configurations = [DigitConfig.load_config(self.config, x, 0) for x in "123 7583847 475845 5849"]
         digits = DigitSequence(configs=configurations, augmentations=[SpeckleNoise()])
         array, annotation = digits.data()
         test_annotations(array, annotation)
         self.assertTrue(np.unique(array).shape[0] > 2)
 
     def test_poisson_noise(self):
-        configurations = [DigitConfig.load_config(self.config, x) for x in "123 7583847 475845 5849"]
+        configurations = [DigitConfig.load_config(self.config, x, 0) for x in "123 7583847 475845 5849"]
         digits = DigitSequence(configs=configurations)
         array, annotation = digits.data()
         test_annotations(array, annotation)
 
-        configurations = [DigitConfig.load_config(self.config, x) for x in "123 7583847 475845 5849"]
+        configurations = [DigitConfig.load_config(self.config, x, 0) for x in "123 7583pkc 847 475845 5849"]
         digits = DigitSequence(configs=configurations, augmentations=[PoissonNoise()])
         array, annotation = digits.data()
         test_annotations(array, annotation)
         self.assertTrue(np.unique(array).shape[0] > 2)
 
     def test_multiple_noise(self):
-        configurations = [DigitConfig.load_config(self.config, x) for x in "123 7583847 475845 5849"]
+        configurations = [DigitConfig.load_config(self.config, x, 0) for x in "123 75838ncjd 47 475845 5849"]
         digits = DigitSequence(configs=configurations)
         array, annotation = digits.data()
         test_annotations(array, annotation)
 
-        configurations = [DigitConfig.load_config(self.config, x) for x in "123 7583847 475845 5849"]
+        configurations = [DigitConfig.load_config(self.config, x, 0) for x in "123 7583847 475845 5849"]
         digits = DigitSequence(configs=configurations,
                                augmentations=[PoissonNoise(), GaussianNoise(), SPNoise(), SpeckleNoise()])
         array, annotation = digits.data()
